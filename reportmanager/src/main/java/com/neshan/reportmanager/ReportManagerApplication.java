75c42d8e01c9938de6486ae74f715005e2f0b415
hossein <h.amirabadi.n@gmail.com>
2022-08-10 19:58:33 +0430

    initial commit

--- reportmanager/src/main/java/com/neshan/reportmanager/ReportManagerApplication.java
+++ reportmanager/src/main/java/com/neshan/reportmanager/ReportManagerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ReportmanagerApplication {
+public class ReportManagerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ReportmanagerApplication.class, args);
+        SpringApplication.run(ReportManagerApplication.class, args);
     }
 
 }
